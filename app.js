c4e8e320bb0c60f3eb8b2b0b6018f9af1e934376
Shreyas4545 <shreyasjakati66@gmail.com>
2023-01-18 15:17:01 +0530

    Chanes

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(morgan('combined', { stream: accessLogStream }));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(json());
 
